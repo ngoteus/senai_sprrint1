1d2e7750ba8cd942d8765c8f3e7134cf4139a205
Mateus <mateusmouraferreira8@gmail.com>
2023-08-04 08:26:43 -0300

    alteracoes 04-08

--- exercicio_1.3/DQL_Exercicio_1_3.sql
+++ exercicio_1.3/DQL_Exercicio_1_3.sql
@@ -2,8 +2,46 @@
 
 USE Exercicio_1_3;
 
-SELECT * FROM Veterinario
+-- listar todos os veterinários (nome e CRMV) de uma clínica (razão social)
 
+
+SELECT 
+	Veterinario.NomeVeterinario, Veterinario.CRMV
+FROM 
+	Veterinario
+WHERE Veterinario.IdClinica= 1
+
+SELECT * FROM Pets
+LEFT JOIN Dono ON Dono.IdDono = Pets.IdDono
+
+SELECT 
+	Atendimento.IdAtendimento AS NumeroAtendimento,
+	Dono.NomeDono AS NomeDono,
+	Pets.NomePet,
+	Pets.IdTipoDePet,
+	Pets.IdRaca, 
+	Veterinario.NomeVeterinario,
+	Clinica.NomeClinica
+
+FROM
+	Atendimento
+LEFT JOIN Veterinario
+	ON Veterinario.IdVeterinario = Atendimento.IdVeterinario
+LEFT JOIN Pets
+	ON Pets.IdPets = Atendimento.IdPets
+LEFT JOIN Dono
+	ON Dono.IdDono = Pets.IdDono
+LEFT JOIN Clinica
+	ON Clinica.IdClinica = Veterinario.IdClinica
+
+	
+	
+
+
+
+
+SELECT * FROM Clinica
+SELECT * FROM Veterinario
 
 
 
